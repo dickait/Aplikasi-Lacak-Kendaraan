e1011b4a9556c68e228ad4d194529c7186bdc3de
Aziz Sudrajat <azizsudrajat95@gmail.com>
2016-07-26 22:47:25 +0700

    add all ionic-material bmkg

---
+++ www/js/app.js
@@ -0,0 +1,104 @@
+// Ionic Starter App
+
+// angular.module is a global place for creating, registering and retrieving Angular modules
+// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
+// the 2nd parameter is an array of 'requires'
+var app = angular.module('app', ['ionic', 'ionic-material', 'app.services', 'ngMap']);
+
+app.run(function ($ionicPlatform) {
+    $ionicPlatform.ready(function () {
+        // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+        // for form inputs)
+
+        if (window.cordova && window.cordova.plugins.Keyboard) {
+            cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+        }
+        if (window.StatusBar) {
+            StatusBar.styleDefault();
+        }
+    });
+})
+
+app.config(function ($stateProvider, $urlRouterProvider) {
+    $stateProvider
+
+    .state('app', {
+        url: '/app',
+        abstract: true,
+        templateUrl: 'templates/menu.html',
+        controller: 'AppCtrl'
+    })
+
+    .state('app.GempaDirasakan', {
+        url: '/GempaDirasakan',
+        views: {
+            'menuContent': {
+                templateUrl: 'templates/GempaDirasakan.html',
+                controller: 'GempaDirasakanController'
+            }
+        }
+    })
+
+    .state('app.SkalaIntesitas', {
+        url: '/SkalaIntesitas',
+        views: {
+            'menuContent': {
+                templateUrl: 'templates/SkalaIntesitasGempabumi.html',
+                controller: 'SkalaIntensitasGempabumi'
+            }
+        }
+    })
+
+    .state('app.AntisipasiGempabumi', {
+        url: '/AntisipasiGempabumi',
+        views: {
+            'menuContent': {
+                templateUrl: 'templates/AntisipasiGempabumi.html',
+                controller: 'MotionCtrl'
+            }
+        }
+    })
+
+    .state('app.GempaTerbaru', {
+        url: '/GempaTerbaru',
+        views: {
+            'menuContent': {
+                templateUrl: 'templates/GempaTerbaru.html',
+                controller: 'GempaTerbaruController'
+            }
+        }
+    })
+
+    .state('app.SkalaMMI', {
+        url: '/SkalaMMI',
+        views: {
+            'menuContent': {
+                templateUrl: 'templates/SkalaMMI.html',
+                controller: 'SkalaMMI'
+            }
+        }
+    })
+
+    .state('app.JaringanStasiun', {
+        url: '/JaringanStasiun',
+        views: {
+            'menuContent': {
+                templateUrl: 'templates/JaringanStasiun.html',
+                controller: 'StasiunGempaController'
+            }
+        }
+    })
+
+    .state('app.GempaTerkini', {
+        url: '/GempaTerkini',
+        views: {
+            'menuContent': {
+                templateUrl: 'templates/GempaTerkini5SR.html',
+                controller: 'GempaTerkiniController'
+            }
+        }
+    });
+
+    // if none of the above states are matched, use this as the fallback
+    $urlRouterProvider.otherwise('/app/GempaTerbaru');
+});
